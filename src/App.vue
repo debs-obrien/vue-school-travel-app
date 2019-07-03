06731a7295ed95888d96d9f2a4677efdec4ae9e5
dobrien <dobrien@todopatterson.com>
2019-07-03 07:36:29 +0200

    add router links to app.vue

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,9 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/brazil">Brazil</router-link>
+      <router-link to="/panama">Panama</router-link>
+      <router-link to="/hawaii">Hawaii</router-link>
+      <router-link to="/jamaica">Jamaica</router-link>
     </div>
     <router-view />
   </div>
