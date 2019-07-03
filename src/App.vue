52c94136c02dbe252efea9387e1abe2ebb302b5a
dobrien <dobrien@todopatterson.com>
2019-07-03 07:03:00 +0200

    add a tag

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
+      <a href="about">About A Tag</a>
     </div>
     <router-view />
   </div>
