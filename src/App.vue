bbcd8c0435425fca3c3aacac6d20d7ddd1eb14e8
dobrien <dobrien@todopatterson.com>
2019-07-03 07:03:54 +0200

    remove a tag

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,6 @@
     <div id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
-      <a href="about">About A Tag</a>
     </div>
     <router-view />
   </div>
